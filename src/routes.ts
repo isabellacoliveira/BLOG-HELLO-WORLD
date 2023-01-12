36e059334dc4a715fdfb16016d9d45e7bfc77460
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-01-12 18:50:35 -0300

    adding routes file and excluding some archives

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+
+export default function AppRoutes(){
+    return (
+    //    <BrowserRouter>
+    //         <Routes>
+    //             <Route path="/" element={<NavBar />}/>
+    //         </Routes>
+    //    </BrowserRouter>
+
+    ); 
+}
